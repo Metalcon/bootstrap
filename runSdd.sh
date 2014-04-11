051188cb198b107187107972ad0921356c02e893
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-04-11 11:56:50 +0200

    Now compiling code before running it.

--- runSdd.sh
+++ runSdd.sh
@@ -1,4 +1,5 @@
 source config.sh
 
 cd $SDD_PATH
+mvn compile
 mvn exec:java -Dexec.mainClass="de.metalcon.sdd.StaticDataDelivery" -Dexec.args="$SDD_CONFIG/config.xml"
--- runUms.sh
+++ runUms.sh
@@ -1,4 +1,5 @@
 source config.sh
 
 cd $UMS_PATH
+mvn compile
 mvn exec:java -Dexec.mainClass="de.metalcon.urlmappingserver.UrlMappingServer" -Dexec.args="$UMS_CONFIG/config.txt"
